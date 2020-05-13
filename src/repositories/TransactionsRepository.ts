1d612913d22838bad670dc1fa06f3f4c7d2308d2
Mateus Queiroz <mateus.caldas@hotmail.com>
2020-05-13 17:54:49 +0100

    challenge finalized

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -10,8 +10,36 @@ interface Balance {
 
 @EntityRepository(Transaction)
 class TransactionsRepository extends Repository<Transaction> {
-  public async getBalance(): Promise<Balance> {
-    // TODO
+  public async getBalance(transactions: Transaction[]): Promise<Balance> {
+    const incomes: number[] = [];
+    const outcomes: number[] = [];
+
+    transactions.map(income => {
+      if (income.type === 'income') {
+        incomes.push(income.value);
+      }
+      return null;
+    });
+
+    transactions.map(income => {
+      if (income.type === 'outcome') {
+        outcomes.push(income.value);
+      }
+      return null;
+    });
+
+    const totalIncome = incomes.reduce((acc, curr) => acc + Number(curr), 0);
+    const totalOutcome = outcomes.reduce((acc, curr) => acc + Number(curr), 0);
+
+    const total = totalIncome - totalOutcome;
+
+    const balance = {
+      income: totalIncome,
+      outcome: totalOutcome,
+      total,
+    };
+
+    return balance;
   }
 }
 
